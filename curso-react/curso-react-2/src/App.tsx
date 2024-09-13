c392a91798f3d5bbce2ad2cd295ae04fd1fd7c4a
Vinicius Vieira Carneiro <vinicius.carneiro@aluno.uece.br>
2024-09-12 21:05:19 -0300

    mexendo nas configurações do tailwind

--- curso-react/curso-react-2/src/App.tsx
+++ curso-react/curso-react-2/src/App.tsx
@@ -1,21 +1,13 @@
 //import { useState } from 'react'
-import logo from './assets/react.svg' 
 import Footer from './components/Footer'
 import Header from './components/Header'
 import './output.css'
 
 function App() {
-  //const [count, setCount] = useState(0)
-
-  const name = 'Vinicius'
-
-  const upperName = name.toUpperCase()
 
   return (
     <>
       <Header/>
-      <p>Oi, {upperName}</p>
-      <img src={logo} alt="Minha imagem" />
       <Footer/>
     </>
   )
--- curso-react/curso-react-2/src/components/Header.tsx
+++ curso-react/curso-react-2/src/components/Header.tsx
@@ -8,13 +8,16 @@ function Header() {
     return (
         <>
             <header>
-                <div className='box-border hover:box-content bg-green-500'>
-                    <nav className="bg-lime-800">
-                        <div className="flex justify-between items-center">
-                            <h1 className="text-2xl font-bold">NAVBAR</h1>
-                        </div>
-                    </nav>
+                <div className='grid grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-3 desktop:grid-cols-4'>
+                    <div className='box-border hover:box-content bg-green-500 '>
+                        <nav className="bg-lime-800">
+                            <div className="flex justify-between items-center">
+                                <h1 className="text-2xl font-bold">NAVBAR</h1>
+                            </div>
+                        </nav>
+                    </div>
                 </div>
+
             </header>
         </>
     )
